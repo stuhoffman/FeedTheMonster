d66469972b68b57ccdefdf478d253203e25f1fe4
Stuart Hoffman <stuhoffman@users.noreply.github.com>
2016-03-11 12:12:20 -0800

    Started enabling dragging of the images

---
+++ FeedTheMonster/DragImg.swift
@@ -0,0 +1,38 @@
+//
+//  DragImg.swift
+//  FeedTheMonster
+//
+//  Created by Stuart Hoffman on 3/11/16.
+//  Copyright © 2016 Stuart Hoffman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DragImg: UIImageView {
+    var originalPosition: CGPoint!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+            originalPosition = self.center
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let position = touch.locationInView(self.superview)
+            self.center = CGPointMake(position.x, position.y)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+    
+}
