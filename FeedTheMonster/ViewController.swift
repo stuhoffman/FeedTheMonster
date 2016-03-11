0e6aec91ec46b54da3b364360499989a4baace22
Stuart Hoffman <stuhoffman@users.noreply.github.com>
2016-03-11 12:32:40 -0800

    Added new class for the monster subclassed from uiimageview

--- FeedTheMonster/ViewController.swift
+++ FeedTheMonster/ViewController.swift
@@ -10,23 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var monsterImg: MonsterImg!
     @IBOutlet weak  var foodImg: DragImg!
     @IBOutlet weak var heartImg: DragImg!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var imgArray = [UIImage]()
-        for var x = 1; x <= 4; x++ {
-            let img = UIImage(named: "idle\(x).png")
-            imgArray.append(img!)
-        }
-        
-        monsterImg.animationImages = imgArray
-        monsterImg.animationDuration = 0.8
-        monsterImg.animationRepeatCount = 0
-        monsterImg.startAnimating()
     }
 
 
